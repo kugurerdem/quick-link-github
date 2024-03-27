93ea1997b65631def4ff9089406acd44e7b694a0
kugurerdem <k.ugurerdem@gmail.com>
2024-03-27 21:03:04 +0300

    Enhance parseAndSave.js content script logic

--- content_scripts/parseAndSave.js
+++ content_scripts/parseAndSave.js
@@ -10,21 +10,28 @@ function onPageLoad(request) {
     console.log('Page loaded', request)
     const headerElement = document.body.querySelector('h1.gh-header-title')
 
+    const pageUrl = window.location.href
+
+    if (! /pull|issues\/\d+/.test(pageUrl))
+        return
+
+    const pageType =  /pull\/\d+/.test(pageUrl) ? 'pull' : 'issue'
+
     if (headerElement) {
-        const prTitle = headerElement.querySelector('bdi').innerText
-        const prNumber = headerElement.querySelector('span').innerText
-        const url = window.location.href
+        const pageTitle = headerElement.querySelector('bdi').innerText
+        const pageIndex = headerElement.querySelector('span').innerText
 
-        chrome.storage.local.get(['prs'], function(result) {
-            const prs = result.prs || []
 
-            if ( !prs.some(pr => pr.prNumber === prNumber) ) {
-                prs.push({ prTitle, prNumber, url })
+        chrome.storage.local.get(['recentPages'], function(result) {
+            const recentPages = result.recentPages || []
+
+            if ( !recentPages.some(pr => pr.pageIndex === pageIndex) ) {
+                recentPages.push({ pageTitle, pageIndex, pageType, pageUrl })
             }
-            prs.sort((a, b) => a.prNumber === prNumber ? -1 : 1)
+            recentPages.sort((a, b) => a.pageIndex === pageIndex ? -1 : 1)
 
-            chrome.storage.local.set({ prs }, function() {
-                console.log('Data saved:', prs)
+            chrome.storage.local.set({ recentPages }, function() {
+                console.log('Data saved:', recentPages)
             })
         })
     }

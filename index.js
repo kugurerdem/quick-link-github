744af87e473d7e801a5817d0d83d6ecd484f93cf
kugurerdem <k.ugurerdem@gmail.com>
2024-04-06 23:19:08 +0300

    Make the implementation more dynamic

--- index.js
+++ index.js
@@ -1,104 +1,118 @@
 const
+    {assign} = Object,
+
     pageUrlRegex =
         /github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/(issues|pull)\/[0-9]+$/,
 
-    titleDelimiter = String.fromCharCode(183)
+    titleDelimiter = String.fromCharCode(183),
 
-    main = async () => {
-        const {title, url: pageUrl} = await new Promise(res => {
-            chrome.tabs.query(
-                {active: true, currentWindow: true},
-                ([tab]) => res(tab),
-            )
-        })
+    state = {
+        currentPage: {},
+        recentCopies: [],
+        pageUrl: '',
+        pageTitle: '',
+    },
 
-        if (pageUrlRegex.test(pageUrl)) {
+    init = async () => {
+        const
+            {title: pageTitle, url: pageUrl} = await new Promise(res => {
+                chrome.tabs.query(
+                    {active: true, currentWindow: true},
+                    ([tab]) => res(tab),
+                )
+            })
+
+
+        if ( pageUrlRegex.test(pageUrl) ) {
             const
-                parts = title.split(titleDelimiter),
+                parts = pageTitle.split(titleDelimiter),
                 // ^ Title is delimited by the character '·' (183), with having
                 // the form <issue or pr name> ' · ' <page index> ' · ' <repo
                 // name>.
 
                 [_pageIndex, repoName] = parts.slice(-2),
                 pageIndex = _pageIndex.match(/\d+/)[0],
-                pageTitle = parts.slice(0, -2).join(titleDelimiter)
+                pageHeader = parts.slice(0, -2).join(titleDelimiter)
                 // ^ Since issue or pr name can contain the delimiter character,
                 // we split the title by the delimiter and take the last two
                 // parts as the page index and repo name.
                 // And then we join the rest of the parts as the page title.
 
-                pageType = pageUrl.includes('Issue') ? 'pr' : 'issue',
+                pageType = pageUrl.includes('Issue') ? 'pr' : 'issue'
 
-            document.body.appendChild(createTitleElement('Copy from this page'))
-
-            document.body.appendChild(
-                createPageElement({
-                    pageTitle,
-                    pageType,
-                    pageIndex,
-                    pageUrl,
-                    repoName,
-                })
-            )
-
-            document.body.appendChild(
-                document.createElement('hr')
-            )
+            assign(state.currentPage, {
+                pageHeader, pageType, pageIndex, repoName,
+            })
         }
 
-        document.body.appendChild(createTitleElement('Previously copied'))
+        assign(state.currentPage, {pageTitle, pageUrl})
 
-        const {recentCopies} = await chrome.storage.local.get('recentCopies')
-        if (!recentCopies) {
-            return
+        state.recentCopies = (
+            await chrome.storage.local.get('recentCopies')
+        )['recentCopies'] || []
+        // TODO: This is ugly.
+
+        render()
+    },
+
+    render = (id, component, ...componentArgs) => {
+        const root = document.getElementById(id || 'root')
+        root.innerHTML = component ? component(...componentArgs) : App(state)
+        setListeners()
+    },
+
+    App = (state) => {
+        return ([
+            state.currentPage.pageType && CopyFromThisPage(state.currentPage),
+            PreviouslyCopied(state.recentCopies),
+        ]
+            .filter(Boolean)
+            .join('<hr>'))
+    },
+
+    CopyFromThisPage = (currentPage) => `
+        <h1>Copy from this page</h1>
+        <ul>
+            ${PageItem(currentPage)}
+        </ul>
+    `,
+
+    PreviouslyCopied = (recentCopies) => `
+        <h1>Previously copied</h1>
+        <ul>
+            ${recentCopies.map(PageItem).join('')}
+        </ul>
+    `,
+
+    PageItem = ({pageHeader, pageType, pageIndex, pageUrl, repoName}) => `
+        <li id="page-item-${pageUrl}">
+            <span>${pageType} ${pageIndex}: ${pageHeader}</span>
+            <button class="copy-button" id="copy-button-${pageUrl}">Copy</button>
+        </li>
+    `,
+
+    setListeners = () => {
+        document.querySelectorAll('.copy-button').forEach(
+            button => button.addEventListener('click', onCopyClick)
+        )
+    },
+
+    onCopyClick = (e) => {
+        const
+            pageUrl = e.target.id.split('copy-button-').slice(1).join(''),
+            pageItem = document.getElementById(`page-item-${pageUrl}`),
+            pageInfoText = pageItem.querySelector('span').innerText
+
+        copyToClipboard(`[${pageInfoText}](${pageUrl})`)
+
+        if (! state.recentCopies.some(({pageUrl: pUrl}) => pUrl === pageUrl)) {
+            state.recentCopies.push(state.currentPage)
         }
 
-        const pageElementList = document.createElement('ul')
-        if (recentCopies)
-            recentCopies.forEach(pageDetails => {
-                const pageElement = createPageElement(pageDetails)
-                pageElementList.appendChild(pageElement)
-            })
+        state.recentCopies.sort((a, _) => a.pageUrl == pageUrl ? -1 : 1)
+        chrome.storage.local.set({ recentCopies: state.recentCopies })
 
-        document.body.appendChild(pageElementList)
-    },
-
-    createTitleElement = (title) => {
-        const titleElement = document.createElement('h1')
-        titleElement.innerText = title
-        return titleElement
-    },
-
-    createPageElement = ({pageTitle, pageType, pageIndex, pageUrl}) => {
-        const pageElement = document.createElement('li')
-
-        const spanElement = document.createElement('span')
-        const pageInfoText = `${pageType} ${pageIndex}: ${pageTitle}`
-        spanElement.innerText = pageInfoText
-        pageElement.appendChild(spanElement)
-
-        const copyButton = document.createElement('button')
-        copyButton.innerText = 'Copy'
-
-        pageElement.appendChild(copyButton)
-
-        copyButton.addEventListener('click', () => {
-            copyToClipboard(`[${pageInfoText}](${pageUrl})`)
-            chrome.storage.local.get('recentCopies', ({recentCopies}) => {
-                recentCopies = recentCopies || []
-
-                if (! recentCopies.some(({pageUrl: pUrl}) => pUrl === pageUrl))
-                    recentCopies.push({pageTitle, pageType, pageIndex, pageUrl})
-
-                recentCopies.sort((a, _) => a.pageUrl == pageUrl ? -1 : 1)
-
-                chrome.storage.local.set({ recentCopies }, () => {
-                    console.log('Data saved:', recentCopies)
-                })
-            })
-        })
-
-        return pageElement
+        render()
     },
 
     copyToClipboard = (text) => {
@@ -116,4 +130,4 @@ const
         document.body.removeChild(textarea);
     }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', init)

c9cb4bd848dda0610861d547d46b93ef1568d8c9
raicem <unalancem@gmail.com>
2024-05-04 17:10:02 +0300

    Run prettier for the first time
    Used npm run lint:fix

--- index.js
+++ index.js
@@ -5,63 +5,63 @@ const pageUrlRegex = /github\.com\/[\w_.-]+\/[\w_.-]+\/(issues|pull)\/[0-9]+$/;
 const titleDelimiter = String.fromCharCode(183);
 
 const state = {
-    currentPage: {},
-    recentCopies: [],
+  currentPage: {},
+  recentCopies: [],
 };
 
 const init = async () => {
-    const { title: pageTitle, url: pageUrl } = await new Promise((res) => {
-        chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) =>
-            res(tab),
-        );
+  const { title: pageTitle, url: pageUrl } = await new Promise((res) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) =>
+      res(tab)
+    );
+  });
+
+  if (pageUrlRegex.test(pageUrl)) {
+    const parts = pageTitle.split(titleDelimiter);
+    // ^ Title is delimited by the character '·' (183), with having
+    // the form <issue or pr name> ' · ' <page index> ' · ' <repo
+    // name>.
+
+    const [_pageIndex, repoName] = parts.slice(-2);
+    const pageIndex = _pageIndex.match(/\d+/)[0];
+    const pageHeader = parts.slice(0, -2).join(titleDelimiter);
+    // ^ Since issue or pr name can contain the delimiter character,
+    // we split the title by the delimiter and take the last two
+    // parts as the page index and repo name.
+    // And then we join the rest of the parts as the page title.
+
+    const pageType = pageUrl.includes("issue") ? "issue" : "pr";
+
+    assign(state.currentPage, {
+      pageHeader,
+      pageType,
+      pageIndex,
+      repoName,
     });
+  }
 
-    if (pageUrlRegex.test(pageUrl)) {
-        const parts = pageTitle.split(titleDelimiter);
-        // ^ Title is delimited by the character '·' (183), with having
-        // the form <issue or pr name> ' · ' <page index> ' · ' <repo
-        // name>.
+  assign(state.currentPage, { pageTitle, pageUrl });
 
-        const [_pageIndex, repoName] = parts.slice(-2);
-        const pageIndex = _pageIndex.match(/\d+/)[0];
-        const pageHeader = parts.slice(0, -2).join(titleDelimiter);
-        // ^ Since issue or pr name can contain the delimiter character,
-        // we split the title by the delimiter and take the last two
-        // parts as the page index and repo name.
-        // And then we join the rest of the parts as the page title.
+  state.recentCopies =
+    (await chrome.storage.local.get("recentCopies"))["recentCopies"] || [];
+  // TODO: This is ugly.
 
-        const pageType = pageUrl.includes('issue') ? 'issue' : 'pr';
-
-        assign(state.currentPage, {
-            pageHeader,
-            pageType,
-            pageIndex,
-            repoName,
-        });
-    }
-
-    assign(state.currentPage, { pageTitle, pageUrl });
-
-    state.recentCopies =
-        (await chrome.storage.local.get('recentCopies'))['recentCopies'] || [];
-    // TODO: This is ugly.
-
-    render();
+  render();
 };
 
 const render = (id, component, ...componentArgs) => {
-    const root = document.getElementById(id || 'root');
-    root.innerHTML = component ? component(...componentArgs) : App(state);
-    setListeners();
+  const root = document.getElementById(id || "root");
+  root.innerHTML = component ? component(...componentArgs) : App(state);
+  setListeners();
 };
 
 const App = (state) => {
-    return [
-        state.currentPage.pageType && CopyFromThisPage(state.currentPage),
-        PreviouslyCopied(state.recentCopies),
-    ]
-        .filter(Boolean)
-        .join('<hr>');
+  return [
+    state.currentPage.pageType && CopyFromThisPage(state.currentPage),
+    PreviouslyCopied(state.recentCopies),
+  ]
+    .filter(Boolean)
+    .join("<hr>");
 };
 
 const CopyFromThisPage = (currentPage) => `
@@ -74,7 +74,7 @@ const CopyFromThisPage = (currentPage) => `
 const PreviouslyCopied = (recentCopies) => `
         <h1>Previously copied</h1>
         <ul>
-            ${recentCopies.map(PageItem).join('')}
+            ${recentCopies.map(PageItem).join("")}
         </ul>
     `;
 
@@ -88,41 +88,41 @@ const PageItem = ({ pageHeader, pageType, pageIndex, pageUrl, repoName }) => `
     `;
 
 const setListeners = () => {
-    document
-        .querySelectorAll('.copy-button')
-        .forEach((button) => button.addEventListener('click', onCopyClick));
+  document
+    .querySelectorAll(".copy-button")
+    .forEach((button) => button.addEventListener("click", onCopyClick));
 };
 
 const onCopyClick = (e) => {
-    const pageUrl = e.target.id.split('copy-button-').slice(1).join(''),
-        pageItem = document.getElementById(`page-item-${pageUrl}`),
-        pageInfoText = pageItem.querySelector('span').innerText;
+  const pageUrl = e.target.id.split("copy-button-").slice(1).join(""),
+    pageItem = document.getElementById(`page-item-${pageUrl}`),
+    pageInfoText = pageItem.querySelector("span").innerText;
 
-    copyToClipboard(`[${pageInfoText}](${pageUrl})`);
+  copyToClipboard(`[${pageInfoText}](${pageUrl})`);
 
-    if (!state.recentCopies.some(({ pageUrl: pUrl }) => pUrl === pageUrl)) {
-        state.recentCopies.push(state.currentPage);
-    }
+  if (!state.recentCopies.some(({ pageUrl: pUrl }) => pUrl === pageUrl)) {
+    state.recentCopies.push(state.currentPage);
+  }
 
-    state.recentCopies.sort((a) => (a.pageUrl == pageUrl ? -1 : 1));
-    chrome.storage.local.set({ recentCopies: state.recentCopies });
+  state.recentCopies.sort((a) => (a.pageUrl == pageUrl ? -1 : 1));
+  chrome.storage.local.set({ recentCopies: state.recentCopies });
 
-    render();
+  render();
 };
 
 const copyToClipboard = (text) => {
-    const textarea = document.createElement('textarea');
+  const textarea = document.createElement("textarea");
 
-    textarea.value = text;
-    textarea.style.position = 'absolute';
-    textarea.style.left = '-9999px';
+  textarea.value = text;
+  textarea.style.position = "absolute";
+  textarea.style.left = "-9999px";
 
-    document.body.appendChild(textarea);
+  document.body.appendChild(textarea);
 
-    textarea.select();
-    document.execCommand('copy');
+  textarea.select();
+  document.execCommand("copy");
 
-    document.body.removeChild(textarea);
+  document.body.removeChild(textarea);
 };
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);

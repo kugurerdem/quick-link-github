80a573366679b487dd7f480ebdcd3d86dc959cbd
raicem <unalancem@gmail.com>
2026-01-05 00:17:23 +0300

    Add icons and tooltips to copy buttons

--- index.js
+++ index.js
@@ -154,19 +154,14 @@ const Contribution = (
         return '<span class="contribution-icon">' + icon + '</span>';
     }
 
-    const isRecentlyCopied =
-        state.recentCopyId && state.recentCopyId.startsWith(id);
-    const copiedType = isRecentlyCopied
-        ? state.recentCopyId.split('__')[1]
-        : null;
-
-    const createBtn = (type, label) => `
+    const createBtn = (type, label, icon, tooltip) => `
         <button class="copy-button"
             data-id="${id}"
             data-type="${type}"
+            title="${tooltip}"
             ${isRecentlyCopied ? 'disabled' : ''}
         >
-            ${copiedType === type ? 'Copied!' : label}
+            ${copiedType === type ? 'Copied!' : `${icon} <span>${label}</span>`}
         </button>
     `;
 
@@ -190,9 +185,9 @@ const Contribution = (
                 </a>
             </div>
             <div class="contribution-actions">
-                ${createBtn('md', 'MD')}
-                ${createBtn('slack', 'Slack')}
-                ${createBtn('docs', 'Docs')}
+                ${createBtn('md', 'MD', MarkdownSvg, 'Copy as Markdown')}
+                ${createBtn('slack', 'Slack', SlackSvg, 'Copy for Slack')}
+                ${createBtn('docs', 'Docs', DocsSvg, 'Copy for Google Docs')}
             </div>
         </li>
     `;
@@ -315,6 +310,26 @@ const PrSvg = `
         <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 
         <path d="M305.8 2.1C314.4 5.9 320 14.5 320 24V64h16c70.7 0 128 57.3 128 128V358.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V192c0-35.3-28.7-64-64-64H320v40c0 9.5-5.6 18.1-14.2 21.9s-18.8 2.3-25.8-4.1l-80-72c-5.1-4.6-7.9-11-7.9-17.8s2.9-13.3 7.9-17.8l80-72c7-6.3 17.2-7.9 25.8-4.1zM104 80A24 24 0 1 0 56 80a24 24 0 1 0 48 0zm8 73.3V358.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V153.3C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80c0 32.8-19.7 61-48 73.3zM104 432a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zm328 24a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
+    </svg>
+`;
+
+const MarkdownSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" fill="currentColor">
+        <path d="M14.85 3H1.15C.52 3 0 3.52 0 4.15v7.7c0 .63.52 1.15 1.15 1.15h13.7c.63 0 1.15-.52 1.15-1.15v-7.7C16 3.52 15.48 3 14.85 3zM7 11H5V5.5L3.5 7 2 5.5V11H0V4h2l1.5 1.5L5 4h2v7zm9 0h-2V7h-2v4h-2V4h2v3h2V4h2v7z"/>
+    </svg>
+`;
+
+const SlackSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 448 512" fill="currentColor">
+        <!-- Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+        <path d="M94.12 315.1c0 25.9-21.16 47.06-47.06 47.06S0 341 0 315.1c0-25.9 21.16-47.06 47.06-47.06h47.06v47.06zm23.72 0c0-25.9 21.16-47.06 47.06-47.06s47.06 21.16 47.06 47.06v117.84c0 25.9-21.16 47.06-47.06 47.06s-47.06-21.16-47.06-47.06V315.1zm47.06-188.98c-25.9 0-47.06-21.16-47.06-47.06S139 32 164.9 32s47.06 21.16 47.06 47.06v47.06H164.9zm0 23.72c25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06H47.06C21.16 243.96 0 222.8 0 196.9s21.16-47.06 47.06-47.06H164.9zm188.98 47.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06h-47.06V196.9zm-23.72 0c0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06V79.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06V196.9zM283.1 385.88c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06v-47.06h47.06zm0-23.72c-25.9 0-47.06-21.16-47.06-47.06 0-25.9 21.16-47.06 47.06-47.06h117.84c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06H283.1z"/>
+    </svg>
+`;
+
+const DocsSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 384 512" fill="currentColor">
+        <!-- Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+        <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/>
     </svg>
 `;
 
